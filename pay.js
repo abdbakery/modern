0d462fc4675d2453bce1445d69fce7b60d00b3ca
abdbakery <abd.2528157@gmail.com>
2026-06-06 21:03:54 +0400

    pay.js

--- pay.js
+++ pay.js
@@ -1,2 +1,5 @@
 window.alert(`20% Discount on first order.`)
 window.alert(`Order will arive in 30 min when clicked submit.`)
+setTimeout(function(){
+    window.location.href = 'index.html';
+}, 5000);
